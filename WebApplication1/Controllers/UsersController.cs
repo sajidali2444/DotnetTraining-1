a4e38cb7181f04c7964bf441d102514d4820e12c
sajid <sajid@SAJIDALI9827>
2024-02-11 20:21:01 +0500

    Add project files.

---
+++ WebApplication1/Controllers/UsersController.cs
@@ -0,0 +1,106 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using WebApplication1.Models;
+
+namespace WebApplication1.Controllers
+{
+    public class UsersController : Controller
+    {
+        
+
+        // GET: Users
+        public ActionResult Index()
+        {
+            var student1 = new Student("Sajid","Ali","03217865510");
+            var student2 = new Student("Sajid", "Ali", "03217865510", "email@email.com");
+            var student3 = new Student();
+            //newStudent.Id = 1;
+            student3.FirstName = "Sajid";
+            student3.LastName = "Ali";
+            student3.Email = "sajid@gmail.com";
+            var newStudentRollNumber = student3.Register(student3);
+
+            var students = new List<Student>();
+            students.Add(student1);
+            students.Add(student2);
+            students.Add(student3);
+            
+          
+            return View(students);
+        }
+
+        // GET: Users/Details/5
+        public ActionResult Details(int id)
+        {
+            return View();
+        }
+
+        // GET: Users/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Users/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Edit/5
+        public ActionResult Edit(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Edit/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Users/Delete/5
+        public ActionResult Delete(int id)
+        {
+            return View();
+        }
+
+        // POST: Users/Delete/5
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Delete(int id, IFormCollection collection)
+        {
+            try
+            {
+                return RedirectToAction(nameof(Index));
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    
+    
+    }
+
+    
+
+}
---
+++ WebApplication1/Models/Student.cs
@@ -0,0 +1,34 @@
+﻿namespace WebApplication1.Models
+{
+    public class Student
+    {
+        public Student() { }
+        public Student(string firstName, string lastName, string phone)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+        }
+        public Student(string firstName, string lastName, string phone, string email)
+        {
+            this.FirstName = firstName;
+            this.LastName = lastName;
+            this.Phone = phone;
+            this.Email = email;
+        }
+
+        public int Register(Student student)
+        {
+            var data = student;
+            return data.Id;
+        }
+        private int Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; } = "ABC as default value";
+        public string? Email { get; set; }
+        public string Phone { get; set; }
+        public char ? Gender { get; set; } //M,F
+        public bool IsActive { get; set; }//true/false
+        
+    }
+}
